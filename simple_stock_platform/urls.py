f08094e86a1761c1b30f63e19405531964ce7a97
ashkan <ashkan.khd.q@gmail.com>
2021-03-03 22:09:33 +0330

    feat(admin): Add fa admin
    Add fa admin.

--- simple_stock_platform/urls.py
+++ simple_stock_platform/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,3 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('symbols/', include('symbols.urls'))
 ]
+
+urlpatterns += i18n_patterns(path('admin/', admin.site.urls))
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
