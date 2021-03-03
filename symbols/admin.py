db17977afeda10538a82b7072c4f9467d6bde648
ashkan <ashkan.khd.q@gmail.com>
2021-03-04 00:03:08 +0330

    fix(models): Add new fields to Symbol
    Add P/E, EPS and company fields to Symbol.
    Add migrations.

--- symbols/admin.py
+++ symbols/admin.py
@@ -1,20 +1,26 @@
+from django.contrib.admin.templatetags.admin_urls import admin_urlname
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib import admin
 from django.http import HttpResponseRedirect
 from django.conf.urls import url
+from django.shortcuts import resolve_url
+from django.utils import timezone
+from django.utils.html import format_html
+from jalali_date import datetime2jalali
 
 from symbols.models import Symbol
-from utility.add_symbols import check_updated_symbols_existence
+from crawler.symbol_creating import add_new_symbols
 
 
 @staff_member_required
 def export(request):
-    check_updated_symbols_existence()
+    add_new_symbols()
     return HttpResponseRedirect(request.META["HTTP_REFERER"])
+
 
 @admin.register(Symbol)
 class SymbolAdmin(admin.ModelAdmin):
-    fields = [
+    symbol_fields = [
         'symbol',
         'open',
         'high',
@@ -22,18 +28,54 @@ class SymbolAdmin(admin.ModelAdmin):
         'adj_close',
         'value',
         'volume',
-        'count',
-        'updated'
+        'count'
     ]
 
-    readonly_fields = ['updated']
+    fields = symbol_fields + [
+        'get_created',
+        'get_updated',
+        'open_link'
+    ]
 
-    list_display = fields
+    readonly_fields = fields
+
+    list_display = symbol_fields + [
+        'get_updated'
+    ]
+
+    def get_created(self, obj):
+        return datetime2jalali(obj.updated).strftime('%Y/%m/%d - %H:%m:%S')
+
+    get_created.short_description = 'تاریخ اضافه شدن'
+    get_created.admin_order_field = 'created'
+
+    def get_updated(self, obj):
+        if obj.created - obj.updated < timezone.timedelta(seconds=1):
+            return 'تا کنون آپدیت نشده است!'
+        return datetime2jalali(obj.updated).strftime('%Y/%m/%d - %H:%m:%S')
+
+    get_updated.short_description = 'آخرین زمان آپدیت'
+    get_updated.admin_order_field = 'updated'
+
+    def open_link(self, obj):
+        url = resolve_url(admin_urlname(obj.__class__._meta, 'change'), obj.id)
+        return format_html(
+            '<a '
+            'href={} role="button" '
+            'style="color:#fff;background-color:#337ab7;border-color:#2e6da4;padding: 10px;border-radius: 8px;">'
+            'باز کردن لینک'
+            '</a>'.format(url)
+        )
+
+    open_link.short_description = 'آپدیت کردن اطلاعات'
 
     def get_urls(self):
         urls = super(SymbolAdmin, self).get_urls()
         my_urls = [url(r"^export/$", export), ]
         return my_urls + urls
+
+    def has_add_permission(self, request):
+        return False
 
     class Meta:
         model = Symbol
--- symbols/models/symbol.py
+++ symbols/models/symbol.py
@@ -5,11 +5,18 @@ from utility.models import null_blank, BaseHistoryModel
 
 class Symbol(BaseHistoryModel):
     symbol = models.CharField(max_length=255, verbose_name='نماد', unique=True)
+    company_name = models.CharField(max_length=255, verbose_name='نام شرکت', **null_blank())
+    url = models.URLField(verbose_name='آدرس صفحه سهم', **null_blank())
+
+    eps = models.IntegerField(verbose_name='eps', **null_blank())
+    p_e_ratio = models.FloatField(verbose_name='P/E', **null_blank())
+    group_p_e_ratio = models.FloatField(verbose_name='Group P/E', **null_blank())
 
     open = models.PositiveIntegerField(verbose_name='اولین', **null_blank())
     high = models.PositiveIntegerField(verbose_name='بیشترین', **null_blank())
     low = models.PositiveIntegerField(verbose_name='کمترین', **null_blank())
     adj_close = models.PositiveIntegerField(verbose_name='پایانی', **null_blank())
+    close = models.PositiveIntegerField(verbose_name='آخرین معامله', **null_blank())
 
     value = models.PositiveBigIntegerField(verbose_name='ارزش معاملات', **null_blank())
     volume = models.PositiveBigIntegerField(verbose_name='حجم معاملات', **null_blank())
